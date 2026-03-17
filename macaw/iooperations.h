113fa8b59a84e02732084b75abaff95efabd46e1
Rafek731 <raf.mazur.000@gmail.com>
2026-03-17 12:59:06 +0100

    deleted unnecessarry files

--- macaw/iooperations.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <filesystem>
-
-namespace macaw {
-    typedef uint16_t m_int;
-    void read_words(const std::filesystem::path&& path);
-    void write_to_file();
-    int check_file_correctness(const std::filesystem::path&& path);
-}
