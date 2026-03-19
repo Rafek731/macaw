69d39975dd4c41a023e6b27a53d2076a9f1f24e2
Rafek731 <raf.mazur.000@gmail.com>
2026-03-19 11:01:34 +0100

    Added comments

--- macaw/macaw.h
+++ macaw/macaw.h
@@ -8,60 +8,81 @@
 #define MAX_LETTERS 12
 
 namespace macaw {
+    // checks if string consists of only characters
     bool alphastr(const std::string &s);
 
+
+    // namespace for functins that normalize vector probabilities
     namespace normalize {
         void linear(std::vector<double>& v);
         void softmax(std::vector<double>& v);
     }
-    
+
     // TODO: better log2?
     double entropy(const std::vector<double>& dist);
 
+    /**
+     * @brief
+     * function that sorts vector by indieces in descending order
+     * 
+     * @details 
+     * let's say we've got array [4, 5, 2, 9]
+     * then sorted indieces list would look like this : [3, 1, 0, 2] 
+     * because
+     * 
+     * @param v 
+     * vector to normalize
+     * 
+     * @returns
+     * indieces - vector of sorted indieces
+     */
     template <typename T>
     std::vector<size_t> argsort(const std::vector<T>& v) {
         std::vector<size_t> indices(v.size());
-
         std::iota(indices.begin(), indices.end(), 0);
-
         std::sort(indices.begin(), indices.end(), [&v](size_t i1, size_t i2) { return v[i1] > v[i2]; });
 
         return indices;
     }
 
     /**
-     * words_file_
-     *      path to file with valid wordle words
+     * @brief
+     * Base class for all wordle sovers. 
      * 
-     * words_
-     *      vector of valid wordle words
+     * @details 
+     * It contains functionality like checking if the file
+     * with valid words is in correct format, loading valid words to memory and remembering
+     * length of words of currently solved wordle
      * 
-     * file_correct_() 
-     *      should return true if file is correct else it throws corresponding errors
+     * @param words_file_
+     * path to file with valid wordle words
      * 
-     * read_words_() 
-     *      should only be accessible if `words_file_` is a path to existing file in valid format
+     * @param words_
+     * vector of valid wordle words
      * 
-     * make_pattern(guess, match)
-     *      creates trinary representation of wordle pattern between given words
+     * @param best_words_
+     * holds sorted indieces for best words
      * 
-     * letters_
-     *      holds length of words in 
+     * @param file_correct_() 
+     * should return true if file is correct else it throws corresponding errors
      * 
-     * number_of_patterns
-     *      number of possible wordle patterns (just 3 ** letters_)
+     * @param read_words_() 
+     * should only be accessible if `words_file_` is a path to existing file in valid format
      * 
-     * words_file()
-     *      returns words_file_
+     * @param make_pattern(guess, match)
+     * creates trinary representation of wordle pattern between given words
      * 
-     * words()
-     *      returns const reference to words_
+     * @param words_file()
+     * returns words_file_
      * 
-     * change_words_path()
-     *      changes selected flile with valid words and loads it
+     * @param words()
+     * returns const reference to words_
      * 
-     * load_words()
-     *      checks file calls file_correct_() and read_words()
+     * @param change_words_path()
+     * changes selected flile with valid words and loads it
+     * 
+     * @param load_words()
+     * checks file calls file_correct_() and read_words()
      */
     class Guesser {
         protected:
@@ -74,16 +95,15 @@ namespace macaw {
         
         
         public:
-        unsigned int letters_;
-        unsigned int number_of_patterns;
         explicit Guesser(std::string path) : Guesser(std::filesystem::path(path)) {}
         explicit Guesser(std::filesystem::path path) 
         : words_file_(path)
-        , words_({}) { 
+        , words_({})
+        , best_words_({}) { 
             load_words(path); 
         } 
         
-        ~Guesser();
+        ~Guesser() {};
         
         const std::vector<std::string>& words() const { return words_; }
         std::filesystem::path words_file() const { return words_file_; }
@@ -92,25 +112,41 @@ namespace macaw {
         void load_words(std::filesystem::path path);
     };
 
-    
-    class MacawV1 : public Guesser {
+    /**
+     * @brief
+     * Vesion 1 of wordle solver
+     * 
+     * @details
+     * 
+     * 
+     * @param entropies_
+     * vector holding entropy for each word. Index of word correspond to index if its entropy
+     * 
+     * @param normalization_fn
+     * pointer to normalization function for probabilities
+     * 
+     * @param entropies()
+     * returns entropies_
+     * 
+     * @param calc_entropies()
+     * calculates entropies for all allowed words
+     * 
+     */
+    class Blue : public Guesser {
 
         void (*normalization_fn)(std::vector<double>& v);
 
-        std::vector<size_t> best_words_;
         std::vector<double> entropies_;
 
         public:
-
-        explicit MacawV1(std::filesystem::path path) 
+        explicit Blue(std::filesystem::path path) 
         : Guesser(path)
         , normalization_fn(macaw::normalize::linear) 
-        , entropies_({})
-        , best_words_({}) { 
+        , entropies_({}){ 
             entropies_.resize(words_.size()); 
         }
 
-        ~MacawV1();
+        ~Blue() {};
 
         std::vector<double> entropies() const { return entropies_; }
         
