5839578797e2b4de978b736e9a39041eb235b524
Rafek731 <raf.mazur.000@gmail.com>
2026-03-28 00:51:43 +0100

    added --help

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 import macaw
 import argparse
 from time import sleep
+import os
 
 def clear_terminal():
-    print("\033[H\033[2J", end="")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def main(valid_words_path: str) -> None:
@@ -13,7 +14,6 @@ def main(valid_words_path: str) -> None:
     bot = macaw.Blue(valid_words_path)
  
     print('Done!\n')
-    sleep(0.2)
 
     while True:
         clear_terminal()
@@ -25,7 +25,7 @@ def main(valid_words_path: str) -> None:
             print(f'Your word is \'{top[0][0]}\'!')
             break
 
-        print(f'Best words for now ({len(top)}):')
+        print(f'Best words for now ({len(bot.words())}):')
         
         for i, tup in enumerate(top, start=1):
             print(f'{i}. {tup[0]}: {tup[1]}')
@@ -38,15 +38,10 @@ def main(valid_words_path: str) -> None:
     input("Press Enter to quit")
 
 
-    
-
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Showcase of macaw wordle solver')
-    parser.add_argument('valid_words_path', type=str, help='Pass path to file containing valid words')
+    parser.add_argument('valid_words_path', type=str, help='Path to file containing valid words')
+    parser.add_help = True
+
     args = parser.parse_args()
     main(args.valid_words_path)
-
-    
-
