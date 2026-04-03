aaf0bf5e8a6391ccc948660623e49cbc99d43452
Rafek731 <raf.mazur.000@gmail.com>
2026-04-03 17:03:00 +0200

    Added base Guesser class

--- Guesser.py
+++ Guesser.py
@@ -1,16 +1,33 @@
 from abc import abstractmethod
+from pathlib import Path
+
 from Pattern import Pattern
 
 class Guesser:
-    """Base class for all guesser models to use in macaw project"""
-    @abstractmethod
-    def __init__(self):
-        pass
+    """
+    Base class for all guesser models to use in macaw project
+    
+    It chcecks correctness of given path"""
+
+    def __init__(self, valid_words):
+        if isinstance(valid_words, str):
+            valid_words = Path(valid_words)
+        if not isinstance(valid_words, Path):
+            raise ValueError(f'This is not a path to a file: {valid_words}')
+        if not valid_words.exists() or not valid_words.is_file():
+            raise NameError(f'This file does not exist: {valid_words}')
+        
     
     @abstractmethod
-    def guess_made(guess: str, pattern: str|int|Pattern) -> None:
-        pass
+    def guess_made(self, guess: str, pattern: str|int|Pattern) -> None:
+        raise NotImplementedError('This method should be overrwritten!')
     
     @abstractmethod
-    def top_guesses(n: int = 10) -> list:
-        pass
+    def top_guesses(self, n: int = 10) -> list:
+        raise NotImplementedError('This method should be overrwritten!')
+    
+    @property
+    @abstractmethod
+    def words(self):
+        raise NotImplementedError('This method should be overrwritten!')
+    
